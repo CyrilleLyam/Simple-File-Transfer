fbbe46857c91ba9c601b8868e1d8c3168f74def9
SEANGLAY <devolutionary.wizard@gmail.com>
2025-05-24 11:02:42 +0700

    feat: bootstrap project structure

---
+++ src/main/java/com/seanglay/simple_file_transfer/SimpleFileTransferApplication.java
@@ -0,0 +1,13 @@
+package com.seanglay.simple_file_transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFileTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFileTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seanglay/simple_file_transfer/SimpleFileTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanglay.simple_file_transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFileTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
